308269d2697790642d3508b8b577c59e6ea72b6e
Anna Do <annado@Momo.local>
2010-05-15 14:54:07 -0700

    use scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :lists
+
   map.connect '', :controller => 'lists'
   # The priority is based upon order of creation: first created -> highest priority.
 
